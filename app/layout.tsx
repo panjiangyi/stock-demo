86db57ce279527db28cc62e9c925891c0a7ed93b
James Pan <panjiangyi@gmail.com>
2025-08-06 10:46:55 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,10 +26,8 @@ export default async function RootLayout({
     <html lang="en" className="bg-gray-100 min-h-screen">
       <body className={inter.className}>
         <Header stocks={stocks} />
-        <div>
-          <div className="max-w-6xl mx-auto p-4">
-            <Suspense fallback={<StatementsPageSkeleton />}>{children}</Suspense>
-          </div>
+        <div className="max-w-6xl mx-auto p-4">
+          <Suspense fallback={<StatementsPageSkeleton />}>{children}</Suspense>
         </div>
       </body>
     </html>
