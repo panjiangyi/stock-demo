71e2961907f920bad683cda829ec6cba4fc50b21
James Pan <panjiangyi@gmail.com>
2025-08-06 10:46:43 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,10 @@ export default async function RootLayout({
 }>) {
   const stocks = await queryStockInfo()
   return (
-    <html lang="en">
+    <html lang="en" className="bg-gray-100 min-h-screen">
       <body className={inter.className}>
         <Header stocks={stocks} />
-        <div className="bg-gray-100 min-h-screen">
+        <div>
           <div className="max-w-6xl mx-auto p-4">
             <Suspense fallback={<StatementsPageSkeleton />}>{children}</Suspense>
           </div>
